b72de6311e67f357898b021f4b8ff808966f8b70
Vlad <vlad@hotmail.co.nz>
2017-07-11 21:40:54 -0700

    finished finally! will add styling later spent way too long at this

--- app.rb
+++ app.rb
@@ -5,86 +5,76 @@ enable :sessions
 
   get '/' do
   	start_game
+  	@turns_left = session[:turns_left] 
+  	@used_letters = session[:used_letters]
   	@the_word = session[:the_word]
   	@hide_word = session[:hide_word]
   	redirect '/play'
   end
 
   get '/play' do
+  	@turns_left = session[:turns_left] 
+  	@used_letters = session[:used_letters]
   	@the_word = session[:the_word]
   	@hide_word = session[:hide_word]
   	erb :index
   end
 
-  post '/play' do 
+  post '/play' do
+  	turns
+  	@turns_left = session[:turns_left] 
+  	@used_letters = session[:used_letters]
   	@the_word = session[:the_word]
   	@hide_word = session[:hide_word]
   	correct_letter?(params['letter'])
+
+  	if win? 
+  		redirect '/win'
+  	end 
+
+  	if lose?
+  		redirect '/lose'
+  	end 
+
   	erb :index
+  end 
+
+  get '/win' do 
+  	@the_word = session[:the_word]
+  erb :win	
+  end
+
+  get '/lose' do
+  	@the_word = session[:the_word]
+  	erb :lose
   end 
 
 helpers do
 
   def start_game 
-		#@used_letters = []
-		#@turns_left = 0
+		session[:used_letters] = []
+		session[:turns_left] = 8
 		session[:the_word] = generate_word
 		session[:hide_word] = secret_word		
 	end 
-
-	def play_game
-		loop do
-			system 'clear' 
-			message
-			puts "chances left: #{8 - @turns_left}"
-			puts "Used letters: #{@used_letters}"
-			guess = enter_guess 
-
-
-			if @the_word.include?(guess)
-			  correct_letter?(guess)
-			else 
-				@turns_left += 1  
-			
-			end
-
-			if win?  
-				outcome = "won"
-				outcome_screen(outcome)
-				break
-
-
-			elsif @turns_left >= 8 
-				outcome = "lost"
-				outcome_screen(outcome)
-				break		
-			end 
-			
-		end 	  
+  
+	def turns 
+		if !session[:the_word].include?(params['letter'])   
+			session[:turns_left] -= 1  	
+		end
 	end
+
+	def lose?
+		if @turns_left == 0 
+			return true 
+		end 
+	end   
 
 	def win? 
 		if @hide_word == @the_word 
 			return true 
 		end 
-	end 	
-
-	def outcome_screen(outcome)
-		system 'clear'
-		puts "You have #{outcome}"
-		puts "The word is #{@the_word.join()}"
-		3.times do 
-			print "Restarting the Game..."
-			sleep(6)
-			@used_letters = []
-			@turns_left = 0 
-			@the_word = generate_word 
-			@hide_word = secret_word 
-			play_game
-		end 
-
-	end 	
-
+	end 		
 
 	def generate_word
 		words = File.read('5desk.txt')
@@ -109,18 +99,14 @@ helpers do
 			if l == letter 
 			 @hide_word[i] = letter 
 			end
-		end 	
-	end  
-
-	def enter_guess 
-		valid = false 
-		while valid == false 
-			guess = @player.letter_choice
-			@used_letters.push(guess)
-			if ("a".."z").to_a.include?(guess) && guess.size == 1
-				return guess 
-				valid = true 
-			end 
 		end 
-	end  
+		if @used_letters.include?(letter)
+			@msg = "Its already there"
+			else 
+			@used_letters.push(letter)
+			end 	
+	end 
+
+
+
 end
